9ec3a3dd610563dbbb53de3490dfd2bdcd37b3c7
Francesco Dente <dente.francesco1996@gmail.com>
2021-10-05 18:54:43 +0200

    Create first tests for the Rent class

---
+++ test/EScooter.RentService.UnitTests/Domain/Aggregates/CustomerAggregate/RentTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EScooter.RentService.Domain.Aggregates.CustomerAggregate;
+using Shouldly;
+using Xunit;
+
+namespace EScooter.RentService.UnitTests.Domain.Aggregates.CustomerAggregate
+{
+    public class RentTests
+    {
+        private readonly Guid _scooterId = Guid.NewGuid();
+        private readonly Rent _sut;
+
+        public RentTests()
+        {
+            _sut = Rent.CreateForScooter(_scooterId);
+        }
+
+        [Fact]
+        public void CreateForScooter_ShouldReturnARentInTheInitialState()
+        {
+            _sut.ScooterId.ShouldBe(_scooterId);
+            _sut.Confirmation.ShouldBeEmpty();
+            _sut.Cancellation.ShouldBeEmpty();
+            _sut.End.ShouldBeEmpty();
+        }
+    }
+}
