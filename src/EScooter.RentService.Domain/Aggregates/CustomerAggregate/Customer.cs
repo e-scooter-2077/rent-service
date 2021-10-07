e4555e0158b213fc3afb5e5cc3c0f99c72928d75
Francesco Dente <dente.francesco1996@gmail.com>
2021-10-07 11:34:55 +0200

    Add customer aggregate root

---
+++ src/EScooter.RentService.Domain/Aggregates/CustomerAggregate/Customer.cs
@@ -0,0 +1,16 @@
+﻿using EasyDesk.CleanArchitecture.Domain.Metamodel;
+using EasyDesk.Tools.Options;
+using System;
+
+namespace EScooter.RentService.Domain.Aggregates.CustomerAggregate
+{
+    public class Customer : AggregateRoot<Customer>
+    {
+        public Customer(Guid id, Option<Rent> currentRent) : base(id)
+        {
+            CurrentRent = currentRent;
+        }
+
+        public Option<Rent> CurrentRent { get; private set; }
+    }
+}
