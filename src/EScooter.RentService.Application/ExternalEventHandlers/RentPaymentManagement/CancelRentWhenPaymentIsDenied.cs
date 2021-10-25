3bc3b712f38e58f5c5b0abe8ae1b1855cb03cc45
Francesco Dente <dente.francesco1996@gmail.com>
2021-10-25 19:16:37 +0200

    Handle events from the payment context

---
+++ src/EScooter.RentService.Application/ExternalEventHandlers/RentPaymentManagement/CancelRentWhenPaymentIsDenied.cs
@@ -0,0 +1,49 @@
+﻿using EasyDesk.CleanArchitecture.Application.Data;
+using EasyDesk.CleanArchitecture.Application.ErrorManagement;
+using EasyDesk.CleanArchitecture.Application.Events.ExternalEvents;
+using EasyDesk.CleanArchitecture.Application.Mediator;
+using EasyDesk.CleanArchitecture.Application.Responses;
+using EasyDesk.CleanArchitecture.Domain.Metamodel.Results;
+using EasyDesk.Tools;
+using EScooter.RentService.Domain.Aggregates.CustomerAggregate;
+using System;
+using System.Threading.Tasks;
+
+namespace EScooter.RentService.Application.ExternalEventHandlers.RentPaymentManagement
+{
+    /// <summary>
+    /// An external event published by the rent payment context whenever the initial payment for a rent
+    /// cannot be performed.
+    /// </summary>
+    /// <param name="RentId">The Id of the rent.</param>
+    public record RentPaymentDenied(Guid RentId) : ExternalEvent;
+
+    /// <summary>
+    /// An external event handler that cancels a rent when payment for that rent is denied.
+    /// </summary>
+    public class CancelRentWhenPaymentIsDenied : ExternalEventHandlerBase<RentPaymentDenied>
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CancelRentWhenPaymentIsDenied"/> class.
+        /// </summary>
+        /// <param name="customerRepository">The customer repository.</param>
+        /// <param name="unitOfWork">The unit of work.</param>
+        public CancelRentWhenPaymentIsDenied(
+            ICustomerRepository customerRepository,
+            IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            _customerRepository = customerRepository;
+        }
+
+        /// <inheritdoc/>
+        protected override async Task<Response<Nothing>> Handle(RentPaymentDenied ev)
+        {
+            return await _customerRepository.GetByRent(ev.RentId)
+                .ThenRequire(customer => customer.CancelOngoingRent(new(RentCancellationReason.CreditInsufficient)))
+                .ThenIfSuccess(_customerRepository.Save)
+                .ThenToResponse();
+        }
+    }
+}
---
+++ src/EScooter.RentService.Application/ExternalEventHandlers/RentPaymentManagement/StopRentWhenCreditRunsOut.cs
@@ -0,0 +1,53 @@
+﻿using EasyDesk.CleanArchitecture.Application.Data;
+using EasyDesk.CleanArchitecture.Application.ErrorManagement;
+using EasyDesk.CleanArchitecture.Application.Events.ExternalEvents;
+using EasyDesk.CleanArchitecture.Application.Mediator;
+using EasyDesk.CleanArchitecture.Application.Responses;
+using EasyDesk.CleanArchitecture.Domain.Metamodel.Results;
+using EasyDesk.CleanArchitecture.Domain.Time;
+using EasyDesk.Tools;
+using EScooter.RentService.Domain.Aggregates.CustomerAggregate;
+using System;
+using System.Threading.Tasks;
+
+namespace EScooter.RentService.Application.ExternalEventHandlers.RentPaymentManagement
+{
+    /// <summary>
+    /// An external event published by the rent payment context when a customer's credit runs out during a rent.
+    /// </summary>
+    /// <param name="RentId">The Id of the rent.</param>
+    public record CreditExhaustedForRent(Guid RentId) : ExternalEvent;
+
+    /// <summary>
+    /// An external event handler that stops a rent when a customer's credit runs out during a rent.
+    /// </summary>
+    public class StopRentWhenCreditRunsOut : ExternalEventHandlerBase<CreditExhaustedForRent>
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly ITimestampProvider _timestampProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StopRentWhenCreditRunsOut"/> class.
+        /// </summary>
+        /// <param name="customerRepository">The customer repository.</param>
+        /// <param name="timestampProvider">The timestamp provider.</param>
+        /// <param name="unitOfWork">The unit of work.</param>
+        public StopRentWhenCreditRunsOut(
+            ICustomerRepository customerRepository,
+            ITimestampProvider timestampProvider,
+            IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            _customerRepository = customerRepository;
+            _timestampProvider = timestampProvider;
+        }
+
+        /// <inheritdoc/>
+        protected override async Task<Response<Nothing>> Handle(CreditExhaustedForRent ev)
+        {
+            return await _customerRepository.GetByRent(ev.RentId)
+                .ThenRequire(customer => customer.StopOngoingRent(new(RentStopReason.CreditExhausted, _timestampProvider.Now)))
+                .ThenIfSuccess(_customerRepository.Save)
+                .ThenToResponse();
+        }
+    }
+}
