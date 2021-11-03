80f581e6e4cb0d07fc2f4970373f667725e0762d
Francesco Dente <dente.francesco1996@gmail.com>
2021-11-03 17:07:09 +0100

    Create Scooter model converter

---
+++ src/EScooter.RentService.Infrastructure/DataAccess/Converters/ScooterConverter.cs
@@ -0,0 +1,29 @@
+﻿using EasyDesk.CleanArchitecture.Dal.EfCore.ModelConversion;
+using EasyDesk.Tools.Options;
+using EScooter.RentService.Domain.Aggregates.ScooterAggregate;
+using EScooter.RentService.Infrastructure.DataAccess.Models;
+
+namespace EScooter.RentService.Infrastructure.DataAccess.Converters
+{
+    public class ScooterConverter : IModelConverter<Scooter, ScooterModel>
+    {
+        public Scooter ToDomain(ScooterModel model)
+        {
+            return new Scooter(
+                model.Id,
+                model.OngoingRentId.AsOption(),
+                model.IsEnabled,
+                model.IsOutOfService,
+                model.IsInStandby);
+        }
+
+        public void ApplyChanges(Scooter origin, ScooterModel destination)
+        {
+            destination.Id = origin.Id;
+            destination.OngoingRentId = origin.OngoingRentId.AsNullable();
+            destination.IsEnabled = origin.IsEnabled;
+            destination.IsOutOfService = origin.IsOutOfService;
+            destination.IsInStandby = origin.IsInStandby;
+        }
+    }
+}
---
+++ test/EScooter.RentService.UnitTests/Infrastructure/Converters/ScooterConverterTests.cs
@@ -0,0 +1,67 @@
+﻿using EasyDesk.Testing.MatrixExpansion;
+using EasyDesk.Tools.Options;
+using EScooter.RentService.Domain.Aggregates.ScooterAggregate;
+using EScooter.RentService.Infrastructure.DataAccess.Converters;
+using EScooter.RentService.Infrastructure.DataAccess.Models;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using static EasyDesk.Tools.Options.OptionImports;
+
+namespace EScooter.RentService.UnitTests.Infrastructure.Converters
+{
+    public class ScooterConverterTests
+    {
+        private readonly ScooterConverter _sut = new();
+        private readonly ScooterModel _persistenceModel = new ScooterModel
+        {
+            Id = Guid.NewGuid()
+        };
+
+        [Theory]
+        [MemberData(nameof(ScooterStates))]
+        public void ToDomain_ShouldCopyBasicProperties(
+            bool enabled, bool standby, bool outOfService, Option<Guid> rentId)
+        {
+            _persistenceModel.IsEnabled = enabled;
+            _persistenceModel.IsInStandby = standby;
+            _persistenceModel.IsOutOfService = outOfService;
+            _persistenceModel.OngoingRentId = rentId.AsNullable();
+
+            _sut.ToDomain(_persistenceModel).ShouldSatisfyAllConditions(
+                scooter => scooter.Id.ShouldBe(_persistenceModel.Id),
+                scooter => scooter.IsEnabled.ShouldBe(enabled),
+                scooter => scooter.IsInStandby.ShouldBe(standby),
+                scooter => scooter.IsOutOfService.ShouldBe(outOfService),
+                scooter => scooter.OngoingRentId.ShouldBe(rentId));
+        }
+
+        [Theory]
+        [MemberData(nameof(ScooterStates))]
+        public void ApplyChanges_ShouldCopyBasicProperties(
+            bool enabled, bool standby, bool outOfService, Option<Guid> rentId)
+        {
+            var scooter = new Scooter(Guid.NewGuid(), rentId, enabled, outOfService, standby);
+
+            _sut.ApplyChanges(scooter, _persistenceModel);
+
+            _persistenceModel.ShouldSatisfyAllConditions(
+                model => model.Id.ShouldBe(scooter.Id),
+                model => model.IsEnabled.ShouldBe(enabled),
+                model => model.IsInStandby.ShouldBe(standby),
+                model => model.IsOutOfService.ShouldBe(outOfService),
+                model => model.OngoingRentId.ShouldBe(rentId.AsNullable()));
+        }
+
+        public static IEnumerable<object[]> ScooterStates()
+        {
+            return Matrix
+                .Axis(true, false)
+                .Axis(true, false)
+                .Axis(true, false)
+                .Axis(None, Some(Guid.NewGuid()))
+                .Build();
+        }
+    }
+}
