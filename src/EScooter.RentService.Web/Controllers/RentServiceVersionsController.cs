12f8b453407559751be7f1997e121c4a2dd86647
Francesco Dente <dente.francesco1996@gmail.com>
2021-11-05 13:01:23 +0100

    Create versions controller

---
+++ src/EScooter.RentService.Web/Controllers/RentServiceVersionsController.cs
@@ -0,0 +1,11 @@
+﻿using EasyDesk.CleanArchitecture.Web.Controllers;
+
+namespace EScooter.RentService.Web.Controllers
+{
+    public class RentServiceVersionsController : VersionsController
+    {
+        public RentServiceVersionsController() : base(typeof(Startup))
+        {
+        }
+    }
+}
