696c68ca01a7a12615cfb6fc22d31a1592bc3b55
Francesco Dente <dente.francesco1996@gmail.com>
2021-11-06 19:44:15 +0100

    Implement Rent controller

---
+++ src/EScooter.RentService.Web/Controllers/V_1_0/RentsController.cs
@@ -0,0 +1,31 @@
+﻿using EasyDesk.CleanArchitecture.Web.Controllers;
+using EScooter.RentService.Application.Commands.Rents;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace EScooter.RentService.Web.Controllers.V_1_0
+{
+    public record RequestRentBodyDto(Guid CustomerId, Guid ScooterId);
+
+    public class RentsController : AbstractMediatrController
+    {
+        [HttpPost("rents")]
+        public async Task<IActionResult> RequestRent([FromBody] RequestRentBodyDto body)
+        {
+            var command = new RequestRent.Command(body.CustomerId, body.ScooterId);
+            return await Command(command)
+                .ReturnOk()
+                .MapEmpty();
+        }
+
+        [HttpPut("rents/{rentId}/stop")]
+        public async Task<IActionResult> RequestRent([FromRoute] Guid rentId)
+        {
+            var command = new StopRent.Command(rentId);
+            return await Command(command)
+                .ReturnOk()
+                .MapEmpty();
+        }
+    }
+}
