58f39dd1b65f1d1b67b3404f197d5b617cfb6aa4
Francesco Dente <dente.francesco1996@gmail.com>
2021-11-08 22:08:23 +0100

    Create Scooters controller

---
+++ src/EScooter.RentService.Web/Controllers/V_1_0/ScootersController.cs
@@ -0,0 +1,29 @@
+﻿using EasyDesk.CleanArchitecture.Web.Controllers;
+using EScooter.RentService.Application.Commands.Scooters;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace EScooter.RentService.Web.Controllers.V_1_0
+{
+    public class ScootersController : AbstractMediatrController
+    {
+        [HttpPost("scooters/{scooterId}/enable")]
+        public async Task<IActionResult> EnableScooter([FromRoute] Guid scooterId)
+        {
+            var command = new EnableScooter.Command(scooterId);
+            return await Command(command)
+                .ReturnOk()
+                .MapEmpty();
+        }
+
+        [HttpPost("scooters/{scooterId}/disable")]
+        public async Task<IActionResult> DisableScooter([FromRoute] Guid scooterId)
+        {
+            var command = new DisableScooter.Command(scooterId);
+            return await Command(command)
+                .ReturnOk()
+                .MapEmpty();
+        }
+    }
+}
