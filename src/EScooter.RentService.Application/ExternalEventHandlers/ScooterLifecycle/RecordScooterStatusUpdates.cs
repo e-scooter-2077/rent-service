9f8b78eecf4f212de975e29168f3f521f766beaa
Francesco Dente <dente.francesco1996@gmail.com>
2021-12-28 15:20:43 +0100

    Handle scooter status updates

---
+++ src/EScooter.RentService.Application/ExternalEventHandlers/ScooterLifecycle/RecordScooterStatusUpdates.cs
@@ -0,0 +1,43 @@
+﻿using EasyDesk.CleanArchitecture.Application.Data;
+using EasyDesk.CleanArchitecture.Application.ErrorManagement;
+using EasyDesk.CleanArchitecture.Application.Events.ExternalEvents;
+using EasyDesk.CleanArchitecture.Application.Mediator;
+using EasyDesk.CleanArchitecture.Application.Responses;
+using EasyDesk.CleanArchitecture.Domain.Metamodel.Results;
+using EasyDesk.Tools;
+using EScooter.RentService.Domain.Aggregates.ScooterAggregate;
+using System;
+using System.Threading.Tasks;
+
+namespace EScooter.RentService.Application.ExternalEventHandlers.ScooterLifecycle
+{
+    public record ScooterStatusChanged(Guid Id, bool Standby) : ExternalEvent;
+
+    public class RecordScooterStatusUpdates : ExternalEventHandlerBase<ScooterStatusChanged>
+    {
+        private readonly IScooterRepository _scooterRepository;
+
+        public RecordScooterStatusUpdates(IScooterRepository scooterRepository, IUnitOfWork unitOfWork) : base(unitOfWork)
+        {
+            _scooterRepository = scooterRepository;
+        }
+
+        protected override async Task<Response<Nothing>> Handle(ScooterStatusChanged ev)
+        {
+            return await _scooterRepository.GetById(ev.Id)
+                .ThenIfSuccess(scooter =>
+                {
+                    if (ev.Standby)
+                    {
+                        scooter.EnterStandby();
+                    }
+                    else
+                    {
+                        scooter.LeaveStandby();
+                    }
+                })
+                .ThenIfSuccess(_scooterRepository.Save)
+                .ThenToResponse();
+        }
+    }
+}
