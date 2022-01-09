028b51e37277cc4159bdd03b233fb4d592dbdad9
Francesco Dente <dente.francesco1996@gmail.com>
2022-01-09 14:01:10 +0100

    Take start time for rent from rent payment service

--- src/EScooter.RentService.Application/ExternalEventHandlers/RentPaymentManagement/ConfirmRentWhenPaymentIsAuthorized.cs
+++ src/EScooter.RentService.Application/ExternalEventHandlers/RentPaymentManagement/ConfirmRentWhenPaymentIsAuthorized.cs
@@ -4,8 +4,8 @@ using EasyDesk.CleanArchitecture.Application.Events.ExternalEvents;
 using EasyDesk.CleanArchitecture.Application.Mediator;
 using EasyDesk.CleanArchitecture.Application.Responses;
 using EasyDesk.CleanArchitecture.Domain.Metamodel.Results;
-using EasyDesk.CleanArchitecture.Domain.Time;
 using EasyDesk.Tools;
+using EasyDesk.Tools.PrimitiveTypes.DateAndTime;
 using EScooter.RentService.Domain.Aggregates.RentAggregate;
 using System;
 using System.Threading.Tasks;
@@ -17,7 +17,7 @@ namespace EScooter.RentService.Application.ExternalEventHandlers.RentPaymentMana
     /// for a rent is authorized.
     /// </summary>
     /// <param name="RentId">The Id of the rent.</param>
-    public record RentPaymentAuthorized(Guid RentId) : ExternalEvent;
+    public record RentPaymentAuthorized(Guid RentId, Timestamp StartTime) : ExternalEvent;
 
     /// <summary>
     /// An external event handler that confirms a rent after the payment for that rent has been authorized.
@@ -25,28 +25,24 @@ namespace EScooter.RentService.Application.ExternalEventHandlers.RentPaymentMana
     public class ConfirmRentWhenPaymentIsAuthorized : ExternalEventHandlerBase<RentPaymentAuthorized>
     {
         private readonly IRentRepository _rentRepository;
-        private readonly ITimestampProvider _timestampProvider;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ConfirmRentWhenPaymentIsAuthorized"/> class.
         /// </summary>
         /// <param name="rentRepository">The rent repository.</param>
-        /// <param name="timestampProvider">The timestamp provider.</param>
         /// <param name="unitOfWork">The unit of work.</param>
         public ConfirmRentWhenPaymentIsAuthorized(
             IRentRepository rentRepository,
-            ITimestampProvider timestampProvider,
             IUnitOfWork unitOfWork) : base(unitOfWork)
         {
             _rentRepository = rentRepository;
-            _timestampProvider = timestampProvider;
         }
 
         /// <inheritdoc/>
         protected override async Task<Response<Nothing>> Handle(RentPaymentAuthorized ev)
         {
             return await _rentRepository.GetById(ev.RentId)
-                .ThenRequire(rent => rent.Confirm(new(_timestampProvider.Now)))
+                .ThenRequire(rent => rent.Confirm(new(ev.StartTime)))
                 .ThenIfSuccess(_rentRepository.Save)
                 .ThenToResponse();
         }
